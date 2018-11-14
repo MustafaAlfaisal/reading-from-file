76b7fb716f0ef69599b32aed3fe35118770ef25c
Mustafa Alfaisal <mustafamfaisal@gmail.com>
2018-11-14 16:19:25 +0300

    add empty lines

--- app.js
+++ app.js
@@ -15,15 +15,19 @@ function processFile(data) {
     // count the number of the rows
     let size = _.size(sData)
     console.log('The size of the file is ' + size + ' rows')
+    console.log("");
     // list all flowers that starts with S Letter
     let startsWithS = sData.filter((flower) => flower.startsWith("S"));
     console.log("The flowers that starts with S is: " + "\n" + startsWithS);
+    console.log("");
     // list all flowers that not starts with S Letter
     let nStartsWithS = _.without(sData, startsWithS)
     console.log("The flowers that not starts with S is: " + "\n" + nStartsWithS);
+    console.log("");
     // list all flowers that starts with S Letter
     let startsWithM = sData.filter((flower) => flower.startsWith("M"));
     console.log("The flowers that starts with M is: " + "\n" + startsWithM);
+    console.log("");
     // list all the flower name length is 5
     let length = sData.filter((flower) => flower.length == 5)
     console.log("the flower name that length is 5 char is: " + "\n" + length)
